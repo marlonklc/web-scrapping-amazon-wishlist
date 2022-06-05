382ae8563a981841b33f9045acf7c0af202a78e6
Marlon Klagenberg <marlon.conceicao@hypeflame.com.br>
2022-06-05 02:10:43 -0300

    added route get to root path

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    return res.json({ status: 'UP' });
+});
+
 app.post('/wishlist-report', WishlistController.createReport);
 
 app.listen(PORT, () => {
